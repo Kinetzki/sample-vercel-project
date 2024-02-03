c8a0f8f18c5b0cc6b657e44d25cab5469a01157d
mrFrost <kinetzki2k20@gmail.com>
2024-02-03 22:36:30 +0800

    added method in route

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from werkzeug.urls import url_q
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return "Hello world"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
