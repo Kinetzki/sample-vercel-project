ce6c59bbcd8294ddc88630df2cb6d4938a05f516
mrFrost <kinetzki2k20@gmail.com>
2024-02-04 08:26:01 +0800

    modified message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello world"
+    return "Hello Rhez zellyace25 password: ..."
 
 if __name__ == '__main__':
     app.run()
